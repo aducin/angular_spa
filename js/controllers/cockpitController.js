d42ae664418bf7215fe799ab76b97c5894f60b95
aducin <albert.ducin@gmail.com>
2016-09-03 00:24:25 +0200

    just a little exclusion

--- js/controllers/cockpitController.js
+++ js/controllers/cockpitController.js
@@ -1,14 +1,23 @@
-angular.module('ZappApp.CockpitController', ['tableSort'])
+angular.module('ZappApp.CockpitController', [])
 
 .controller('CockpitController', ["$scope", '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
 	  $scope.active = [];
 	  $scope.active.cockpitActive = true;
 	  $scope.table = [];
+	  $scope.sort = '';
+	  $scope.sortType  = 'text';
+	  $scope.sortReverse  = false;
+	  
 	  
 	  $http.get('http://local.example/json.php')
 	  .then(function(response){
 		  if (response.data.length != 0) {
 			  $scope.table = response.data;
-		  }
+		  }  
 	  })
+	  
+	  $scope.sort = function(name) {
+	      $scope.sortType = name;
+	      $scope.sortReverse = !$scope.sortReverse;
+	  }
 }]);
